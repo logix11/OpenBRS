e0101e54917a7250fa571d67232faf4e933ef240
logix11 <ouka.lotfi@gmail.com>
2025-09-01 18:53:12 +0100

    refactorign the code from scratch

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,40 @@
-use std::{fs::File, path::{self, Path}}; 
+/*use chrono::prelude::Utc;
+//use fileops::archive;
 use proc_mounts::MountIter;
+use std::{
+    fs::File,
+    path::{self, Path},
+};
 use tar::Builder;
-use chrono::prelude::Utc;
 
-mod fileops { 
-    use std::fs::File; use crate::path; use crate::Path; use crate::MountIter; 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /*#[test]
+    fn take_input() {
+        let query = "test";
+
+    }*/
+}
+
+mod fileops {
+    use std::fs::File; use crate::path; use crate::Path; use crate::MountIter;
     use crate::Builder; use crate::Utc;
 
     pub fn find_source(path: &String) -> u8 {
         /*
         This function will search for the given path.
-        If it finds it, and it is a file, it return 1. Otherwise, if is it a 
+        If it finds it, and it is a file, it return 1. Otherwise, if is it a
         directory, it returns 2 (in binary.) Otherwise, it returns a 0.
         */
-        if Path::new(path).is_file() { 
+        if Path::new(path).is_file() {
             return 0b1
         } else if Path::new(path).is_dir() {
             return 0b10
         } else {
             return 0b0
-        } 
-    }
-        
-    pub fn init_destination() {
-        let mut destination:String = String::new();
-        'destination: loop{
-
-            print!("Enter the path to the directory in which you want to backup to (make sure it is a mounted block device) :: ");
-            let path: String = text_io::read!("{}\n"); 
-            destination.push_str(&path);
-            
-            // Parse the /proc/mounts file to ensure that the destination is a valid 
-            // mount point.
-            match find_destination(&destination){
-                true => {
-                    println!("Destination is set.");
-                    break 'destination;
-                },
-                false => {
-                    println!("Invalid path--it either does not exist or is not a 
-                    mount point.");
-                    destination.clear();
-                }
-            }
-            
         }
-
-        // Archiving it. Create a timestamped name for the archive.
-        // The name will be "<destination>/archive-<timestamp>.tar"
-        let name = format!(
-            "{}/archive-{}.tar",
-            destination, 
-            Utc::now().to_string()
-        );
-
-        // Create the archive file then building it as an archive.
-        
     }
 
     pub fn find_destination(path: &String) -> bool {
@@ -77,58 +56,88 @@ mod fileops {
         // Second, we make it an archive
         let mut file: Builder<File> = Builder::new(file);
 
-        // Then, if the source is a file, we append it to the archive
-        // The first argument is the path to the archive.
-        // The second argument is the path to the file.
+        // Then, if the source is a file, we append it to the archive. The first
+        // argument is the path to the archive. The second argument is the path
+        // to the file.
         if path::Path::new(&source).is_file() {
             file.append_file(
-                    "archive", 
+                    "archive",
                     &mut File::open(source.clone()).unwrap()
                 ).unwrap();
-        
+
         // If the source is a directory, then we append the directory, then append
         // its conent.
         } else if Path::new(&source).is_dir() {
             file.append_dir(
-                    "archive", 
+                    "archive",
                     source.clone()
                 ).unwrap();
-    
+
             file.append_dir_all(
-                    "archive", 
+                    "archive",
                     source.clone()
                 ).unwrap();
-    
+
         }
     }
 }
 
 pub fn api(){
+    use crate::fileops;
     let mut source: String = String::new();
-        loop {
+    loop {
 
-            // Reading source
-            print!("Enter the path to the file OR the directory you want to backup (Postscriptum: it MUST be a relative path):: ");
-            
-            // Storing the read value in a buffer
-            let path:String = text_io::read!("{}\n");
-            
-            // Check whether the path is valid or not.
-            let ftype = crate::fileops::find_source(&path);
-            match ftype{
-                0b1 => {
-                    println!("File is found.");
-                    source.push_str(&path);
-                    break;
-                }, 
-                0b10 => {
-                    println!("Directory is found.");
-                    source.push_str(&path);
-                    break},
-                0b0 => println!("Invalid path."),
-                _ => {
-                    // the compiler will not leave me until i put this line, so..
-                }
+        // Reading source
+        print!("Enter the path to the file OR the directory you want to backup (Postscriptum: it MUST be a relative path):: ");
+
+        // Storing the read value in a buffer
+        let path:String = text_io::read!("{}\n");
+
+        // Check whether the path is valid or not.
+        let ftype = fileops::find_source(&path);
+        match ftype{
+            0b1 => {
+                println!("File is found.");
+                source.push_str(&path);
+                break;
+            },
+            0b10 => {
+                println!("Directory is found.");
+                source.push_str(&path);
+                break},
+            0b0 => println!("Invalid path."),
+            _ => {}
+        }
+    }
+    let mut destination:String = String::new();
+
+    loop{
+        print!("Enter the path to the directory in which you want to backup to (make sure it is a mounted block device) :: ");
+        let path: String = text_io::read!("{}\n");
+
+        // Parse the /proc/mounts file to ensure that the destination is a valid
+        // mount point.
+        match fileops::find_destination(&path){
+            true => {
+                println!("Destination is set.");
+                destination.push_str(&path);
+                break;
+            },
+            false => {
+                println!("Invalid path--it either does not exist or is not a
+                mount point.");
+                destination.clear();
             }
-        } 
+        }
+    }
+    // Archiving it. Create a timestamped name for the archive. The name will be
+    // "<destination>/archive-<timestamp>.tar"
+    let name = format!(
+        "{}/archive-{}.tar",
+        destination,
+        Utc::now().to_string()
+    );
+    archive(name, source);
+
 }
+ */
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,127 @@
-use text_io; 
-use std::{fs::File, path::{self, Path}}; 
-use chrono::prelude::Utc;
-extern crate tar; 
+//use chrono::prelude::Utc;
+use std::fs::{self, File};
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::panic;
+use xz::read::XzEncoder;
+//use text_io;
+//use chrono;
 use tar::Builder;
-use chrono;
-extern crate proc_mounts;
-use proc_mounts::MountIter;
+// Need
+
+//extern crate proc_mounts;
+//use proc_mounts::MountIter;
 
 fn main() {
-	// Getting source. First, initialize the `source` to keep it in scope.
-	let mut source: String = String::new();
-	'source: loop {
-		// Reading source
-		print!("Enter the path to the file OR the directory you want to backup (Postscriptum: it MUST be a relative path):: ");
-		
-		// Storing the read value in a buffer
-		let path:String = text_io::read!("{}\n");
-		
-		// pushing the buffer's value to the `source` 
-		source.push_str(&path);
+    // CREATE A NEW ARCHIVE
+    // Set the path
+    let archive_path = "test/backup.tar.xz";
+    // Create the file before turning it to an archive
+    let archive_file = File::create(archive_path);
+    // To replace the unwrap... For now, I just want it to panic
+    let archive_file = match archive_file {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file :: {error}"),
+    };
+    // Build the archive
+    let mut archive = Builder::new(archive_file);
+    //
+    // add a file to the archive
+    archive.append_path(path)
 
-		// Check whether the path is valid or not.
-		if Path::new(&source).is_file() || Path::new(&source).is_dir() {
-			break 'source;
-		}
-		println!("Invalid path.");
-		source.clear();
-	}
 
-	let mut destination:String = String::new();
-	'destination: loop{
-		print!("Enter the path to the directory in which you want to backup to (make sure it is a mounted block device) :: ");
-		let path: String = text_io::read!("{}\n"); 
-		destination.push_str(&path);
-		
-		// Parse the /proc/mounts file to ensure that the destination is a valid 
-		// mount point.
-		for mount in MountIter::new().unwrap(){
-			// pattern matching and to get the Result only, without the Error.
-			if let Ok(mount) = mount {
-				if Path::new(&destination) == mount.dest {
-					break 'destination;
-				}
-			}
-		}
-		println!("Invalid path.");
-		destination.clear();
-	}
+    // Read the file before encoding it
+    let file_to_encode = fs::File::open(archive_path).unwrap();
+    // Buffer it to gain te Read trait
+    let buffer = BufReader::new(file_to_encode);
+    // Encode it
+    let mut encoder = XzEncoder::new(buffer, 9);
+    // prepare the file to write to
+    let mut compressed_file = File::create("{archive_file}.zx").unwrap();
+    // prepare the buffer to read the compressed data
+    let mut buffer = Vec::new();
+    // Push the comressed data to the buffer
+    encoder.read_to_end(&mut buffer).unwrap(); // panic if it fails
+    // Turn the buffer to a &[u8]
+    let buffer: &[u8] = &buffer;
+    compressed_file.write_all(buffer).unwrap(); // panic if it fails
+    /*
+    // Getting source. First, initialize the `source` to keep it in scope.
+    let mut source: String = String::new();
+    'source: loop {
+        // Reading source
+        print!("Enter the path to the file OR the directory you want to backup (Postscriptum: it MUST be a relative path):: ");
 
-	// Archiving it. Create a timestamped name for the archive.
-	// The name will be "<destination>/archive-<timestamp>.tar"
-	let name = format!(
-		"{}/archive-{}.tar",
-		destination, 
-		Utc::now().to_string()
-	);
+        // Storing the read value in a buffer
+        let path:String = text_io::read!("{}\n");
 
-	// Create the archive file then building it as an archive.
-	let archive = File::create(name).unwrap();
-	let mut _archive: Builder<File> = Builder::new(archive);
+        // pushing the buffer's value to the `source`
+        source.push_str(&path);
 
-	if path::Path::new(&source).is_file() {
-		// If the path is a file, append it with the name "archive" and path "path"
-		_archive.append_file(
-				"archive", 
-				&mut File::open(source.clone())
-				.unwrap()
-			).unwrap();
+        // Check whether the path is valid or not.
+        if Path::new(&source).is_file() || Path::new(&source).is_dir() {
+            break 'source;
+        }
+        println!("Invalid path.");
+        source.clear();
+    }
 
-	} else if Path::new(&source).is_dir() {
-		// Otherwise, if the path is a directory append it with the name "archive"
-		// and path "path"
-		_archive.append_dir(
-				"archive", 
-				source.clone()
-			).unwrap();
+    let mut destination:String = String::new();
+    'destination: loop{
+        print!("Enter the path to the directory in which you want to backup to (make sure it is a mounted block device) :: ");
+        let path: String = text_io::read!("{}\n");
+        destination.push_str(&path);
 
-		// Then, we'll need to append its content, too.
-		_archive.append_dir_all(
-				"archive", 
-				source.clone()
-			).unwrap();
+        // Parse the /proc/mounts file to ensure that the destination is a valid
+        // mount point.
+        for mount in MountIter::new().unwrap(){
+            // pattern matching and to get the Result only, without the Error.
+            if let Ok(mount) = mount {
+                if Path::new(&destination) == mount.dest {
+                    break 'destination;
+                }
+            }
+        }
+        println!("Invalid path.");
+        destination.clear();
+    }
 
-	} else {
-		println!("Invalid path"); 
-	}
+    // Archiving it. Create a timestamped name for the archive.
+    // The name will be "<destination>/archive-<timestamp>.tar"
+    let name = format!(
+        "{}/archive-{}.tar",
+        destination,
+        Utc::now().to_string()
+    );
+
+    // Create the archive file then building it as an archive.
+    let archive = File::create(name).unwrap();
+    let mut _archive: Builder<File> = Builder::new(archive);
+
+    if path::Path::new(&source).is_file() {
+        // If the path is a file, append it with the name "archive" and path "path"
+        _archive.append_file(
+                "archive",
+                &mut File::open(source.clone())
+                .unwrap()
+            ).unwrap();
+
+    } else if Path::new(&source).is_dir() {
+        // Otherwise, if the path is a directory append it with the name "archive"
+        // and path "path"
+        _archive.append_dir(
+                "archive",
+                source.clone()
+            ).unwrap();
+
+        // Then, we'll need to append its content, too.
+        _archive.append_dir_all(
+                "archive",
+                source.clone()
+            ).unwrap();
+
+    } else {
+        println!("Invalid path");
+    }
+     */
 }
